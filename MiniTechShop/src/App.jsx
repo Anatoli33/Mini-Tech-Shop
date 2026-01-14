836c4fd304bf5f1aa004dbf544ecfd50f342ec5e
Anatoli33 <hadjiev1215@gmail.com>
2026-01-14 12:08:10 +0200

    Add hero section

--- MiniTechShop/src/App.jsx
+++ MiniTechShop/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
+import Hero from './Hero';
 
 function App() {
 
 
   return (
-    <>
-      <h1>Mini Tech Shop</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore a iure quos eius numquam harum. Laudantium nisi est quos nulla, ab ex quo accusantium ducimus minima nesciunt itaque veritatis molestias.</p>
-    </>
+    <Hero/>
   )
 }
 
---
+++ MiniTechShop/src/Hero.jsx
@@ -0,0 +1,16 @@
+import "./hero.css"
+ 
+const Hero = ()=>{
+    return(
+    <section id="home" className="hero">
+    <h2>Дoбре дошъл в Mini Tech Shop</h2>
+      <p>
+         Открий най-новите и качествени технологични джаджи на страхотни цени – само в Mini Tech Shop!
+      </p>
+      <a href="">
+        <button>Виж продукти</button>
+      </a>
+    </section> 
+    )
+};
+export default Hero;
