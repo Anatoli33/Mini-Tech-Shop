817ebf4b8591fefcd71f3e7e5d2e28d119c482aa
Anatoli33 <hadjiev1215@gmail.com>
2026-01-15 18:48:54 +0200

    Fixed navigation

--- MiniTechShop/src/Navigation.jsx
+++ MiniTechShop/src/Navigation.jsx
@@ -1,13 +1,29 @@
+import { NavLink } from "react-router-dom";
 import "./navigation.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1 className="logo">Mini Tech Shop</h1>
+
       <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#products">Product List</a></li>
-        <li><a href="#cart">Cart</a></li>
+        <li>
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/products">
+            Product List
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/cart">
+            Cart
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
