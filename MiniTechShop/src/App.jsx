66e2b6c2ea8ace731e7055404a82bcdce1bee8b5
Anatoli33 <hadjiev1215@gmail.com>
2026-01-15 18:24:05 +0200

    Add react router and products section

--- MiniTechShop/src/App.jsx
+++ MiniTechShop/src/App.jsx
@@ -2,14 +2,20 @@ import './App.css'
 import Hero from './Hero';
 import Footer from './Footer';
 import Navbar from './Navigation.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Products from './Products';
 
 function App() {
 
 
   return (
+    
     <div className='app-layout'>
     <Navbar/>
-    <Hero/>
+    <Routes>
+      <Route path="/" element={<Hero />} />
+      <Route path="/products" element={<Products />} />
+    </Routes>
     <Footer/>
     </div>
   )
---
+++ MiniTechShop/src/Products.jsx
@@ -0,0 +1,14 @@
+
+ 
+const Products = ()=>{
+    return(
+    <section id="products" className="products">
+    <h2>Mini Tech Shop</h2>
+    <article>
+        <h3>Контролер за PS5</h3>
+        <img src="https://cdn.ozone.bg/media/catalog/product/k/o/kontroler_dualsense_edge_wireless_controller_1673274086_0.jpg" alt="controller PS5" />
+    </article>
+    </section> 
+    )
+};
+export default Products;
