c58534001a842015eed8c384b849af813953e22b
Anatoli33 <hadjiev1215@gmail.com>
2026-01-17 19:33:54 +0200

    Export products as a data

---
+++ MiniTechShop/src/data/product.js
@@ -0,0 +1,33 @@
+export const products = [
+  {
+    id: 1,
+    title: "Controller for PS5",
+    price: "$69.99",
+    img: "https://cdn.ozone.bg/media/catalog/product/k/o/kontroler_dualsense_edge_wireless_controller_1673274086_0.jpg",
+  },
+  {
+    id: 2,
+    title: "Gaming mouse Razer",
+    price: "$59.99",
+    img: "https://laptop.bg/system/images/189888/original/RZ0102540100R3M1.jpg",
+    description: "Lorem ipsum fdsafdsa fdsfsasd fasdfdasdfasd fasd gafsdgv d dfghhgdfhd",
+  },
+  {
+    id: 3,
+    title: "Gaming keyboard Logitech",
+    price: "$89.99",
+    img: "https://m.media-amazon.com/images/I/51K1mE5uVyL._AC_UF894,1000_QL80_.jpg",
+  },
+    {
+    id: 4,
+    title: "Gaming keyboard Razer",
+    price: "$89.99",
+    img: "https://m.media-amazon.com/images/I/71sXzUqI9XL._AC_UF894,1000_QL80_.jpg",
+  },
+    {
+    id: 5,
+    title: "Gaming headset HyperX",
+    price: "$89.99",
+    img: "https://m.media-amazon.com/images/I/71ltsViEA8L._AC_UF894,1000_QL80_.jpg",
+  },
+];
