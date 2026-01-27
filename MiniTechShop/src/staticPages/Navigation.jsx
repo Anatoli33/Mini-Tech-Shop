b2f6fd05e46a66445fcdc2d0075ff7ebe526a8d8
Anatoli33 <hadjiev1215@gmail.com>
2026-01-27 10:43:48 +0200

    Responsive navbar

--- MiniTechShop/src/staticPages/Navigation.jsx
+++ MiniTechShop/src/staticPages/Navigation.jsx
@@ -1,26 +1,41 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import "./navigation.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <nav className="navbar">
       <h1 className="logo">Mini Tech Shop</h1>
 
-      <ul className="nav-links">
+      {/* Hamburger */}
+      <div
+        className="menu-toggle"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
         <li>
-          <NavLink to="/" end>
+          <NavLink to="/" end onClick={closeMenu}>
             Home
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="/products">
+          <NavLink to="/products" onClick={closeMenu}>
             Product List
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="/cart">
+          <NavLink to="/cart" onClick={closeMenu}>
             Cart
           </NavLink>
         </li>
