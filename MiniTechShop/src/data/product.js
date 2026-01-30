61a94ac2fca4e8c6e47e3dde23ae8f48d13d8ea1
Anatoli33 <hadjiev1215@gmail.com>
2026-01-30 15:24:38 +0200

    Add more products

--- MiniTechShop/src/data/product.js
+++ MiniTechShop/src/data/product.js
@@ -4,30 +4,63 @@ export const products = [
     title: "Controller for PS5",
     price: "69.99",
     img: "https://cdn.ozone.bg/media/catalog/product/k/o/kontroler_dualsense_edge_wireless_controller_1673274086_0.jpg",
+    description:
+      "Официален безжичен контролер DualSense за PlayStation 5 с адаптивни спусъци, хаптична обратна връзка и вграден микрофон.",
   },
   {
     id: 2,
     title: "Gaming mouse Razer",
     price: "59.99",
     img: "https://laptop.bg/system/images/189888/original/RZ0102540100R3M1.jpg",
-    description: "Lorem ipsum fdsafdsa fdsfsasd fasdfdasdfasd fasd gafsdgv d dfghhgdfhd",
+    description:
+      "Прецизна геймърска мишка Razer с висок DPI сензор, RGB подсветка и ергономичен дизайн за дълги гейминг сесии.",
   },
   {
     id: 3,
     title: "Gaming keyboard Logitech",
     price: "89.99",
     img: "https://m.media-amazon.com/images/I/51K1mE5uVyL._AC_UF894,1000_QL80_.jpg",
+    description:
+      "Механична гейминг клавиатура Logitech с бързи суичове, RGB подсветка и здрава алуминиева конструкция.",
   },
-    {
+  {
     id: 4,
     title: "Gaming keyboard Razer",
     price: "89.99",
     img: "https://m.media-amazon.com/images/I/71sXzUqI9XL._AC_UF894,1000_QL80_.jpg",
+    description:
+      "Razer механична клавиатура с тактилни суичове, Chroma RGB и оптимизирана за eSports производителност.",
   },
-    {
+  {
     id: 5,
     title: "Gaming headset HyperX",
     price: "89.99",
     img: "https://m.media-amazon.com/images/I/71ltsViEA8L._AC_UF894,1000_QL80_.jpg",
+    description:
+      "HyperX геймърски слушалки с кристално чист звук, комфортни мемори пяни и шумопотискащ микрофон.",
+  },
+  {
+    id: 6,
+    title: "Gaming monitor ASUS 144Hz",
+    price: "249.99",
+    img: "https://s13emagst.akamaized.net/products/59112/59111310/images/res_2253197cab03cb685f2f313f9d386587.jpg",
+    description:
+      "24-инчов гейминг монитор ASUS с 144Hz честота, 1ms време за реакция и FreeSync технология за плавен геймплей.",
+  },
+  {
+    id: 7,
+    title: "Gaming chair DXRacer",
+    price: "299.99",
+    img: "https://s13emagst.akamaized.net/products/84946/84945720/images/res_dfc109d7e3c24bf0b24fb3c54f76ead4.jpg?width=720&height=720&hash=A1242698D9355692CE77AE003FEA4C0A",
+    description:
+      "Ергономичен гейминг стол DXRacer с лумбална опора, регулируема височина и здрава метална конструкция.",
+  },
+  {
+    id: 8,
+    title: "Webcam Logitech Full HD",
+    price: "79.99",
+    img: "https://p.jarcomputers.com/1000x1000/7d/FWEBLOGITECHC920_1000x1000.jpg",
+    description:
+      "Logitech Full HD уеб камера с 1080p резолюция, автофокус и отличен звук за стрийминг и видео разговори.",
   },
 ];
